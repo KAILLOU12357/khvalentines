1e9f2db75470cd5686c4a0ecb7612b34e0c3531b
KAILLOU12357 <kahouhong@gmail.com>
2025-02-12 22:05:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ const messages = [
 let messageIndex = 0;
 
 function handleNoClick() {
-    const noButton = document.querySelector('.你敢？-button');
-    const yesButton = document.querySelector('.愿意-button');
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
     noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1) % messages.length;
+    messageIndex = (messageIndex + 10) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    yesButton.style.fontSize = `${currentSize * 3}px`;
 }
 
 function handleYesClick() {
